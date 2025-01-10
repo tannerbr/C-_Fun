77044cdaadec0eacddcdfd9e779ace1a64e5a46e
tannerbr <tannerjbriggs@hotmail.com>
2025-01-10 10:07:14 -0700

    Add project files.

---
+++ PrintClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Fun
+{
+    internal class PrintClass
+    {
+        private string language = "";
+
+        // Constructor (only run once)
+        public PrintClass()
+        {
+            language = "EN";
+        }
+        public PrintClass (string langHolder)
+        {
+            language = langHolder;
+        }
+        public void PrintName(string username)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + username);
+            }
+
+            if (language == "SW")
+            {
+                System.Console.WriteLine("Niaje, " + username + " unaongea Kiswahili?");
+            }
+            
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+using C__Fun;
+internal class Program
+{
+    private static void Main(string[] args) {
+        string name = "";
+
+        PrintClass printc = new PrintClass("SW");
+
+        System.Console.WriteLine("Please Enter Your Name: ");
+        name = System.Console.ReadLine();
+
+        printc.PrintName(name);
+
+    }
+}
